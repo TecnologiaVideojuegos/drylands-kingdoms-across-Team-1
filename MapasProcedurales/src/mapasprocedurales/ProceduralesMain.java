c22497e7e13f50b815cd688d77dccb05c5b7c83a
FairLight <marino@gamesfera.com>
2018-04-16 23:24:21 +0200

    anadida toda la api

--- MapasProcedurales/src/mapasprocedurales/ProceduralesMain.java
+++ MapasProcedurales/src/mapasprocedurales/ProceduralesMain.java
@@ -5,14 +5,63 @@
  */
 package mapasprocedurales;
 
+
+
+import org.mapeditor.core.*;
+import org.mapeditor.io.TMXMapReader;
+import org.mapeditor.io.TMXMapWriter;
+import org.mapeditor.util.BasicTileCutter;
+
+import java.io.IOException;
+
 /**
  *
  * @author FairLight
  */
 public class ProceduralesMain {
     public static void main(String[] argv) {
-        int alto=5,ancho=10,salas=5;
-        Celula[][] mapa = MetodosProcedural.GenerarMapaLogico(ancho, alto, salas);
-        MetodosProcedural.mostrarMapa(mapa, ancho, alto);
+        int altomapa=5,anchomapa=10,salas=5;
+        int altocelda=20,anchocelda=20;
+        Celula[][] mapalogico = MetodosProcedural.GenerarMapaLogico(anchomapa, altomapa, salas);
+        MetodosProcedural.mostrarMapa(mapalogico, anchomapa, altomapa);
+        
+        
+        //Creamos el mapagrafico, con las medidas
+        Map mapagrafico = new Map(anchomapa*anchocelda,altomapa*altocelda);
+        TileSet tileset = new TileSet();
+        tileset.setTileWidth(16);
+        tileset.setTileHeight(16);
+        try{
+            tileset.importTileBitmap("ficheros/minas.png", new BasicTileCutter(16,16,0,0)); //importamos el tile
+        }
+        catch(IOException e){
+            System.out.print("Error al abrir el fichero");
+        }
+        mapagrafico.addTileset(tileset);
+        
+        
+        //Creamos las capas
+        TileLayer suelo = new TileLayer();
+        TileLayer paredes = new TileLayer();
+        suelo.setName("suelo");
+        paredes.setName("paredes");
+        
+        
+        
+        
+        //Añadimos las capas al mapa
+        mapagrafico.addLayer(suelo);
+        mapagrafico.addLayer(paredes);
+        
+        //Escribimos el fichero
+        TMXMapWriter writer = new TMXMapWriter();
+        writer.settings.layerCompressionMethod = "gzip" ;
+        try{
+            writer.writeMap(mapagrafico, "ficheros/fichero.tmx");
+        }
+        catch(IOException e){
+            System.out.print("Error al escribir el fichero");
+        }
+       
     }
 }
