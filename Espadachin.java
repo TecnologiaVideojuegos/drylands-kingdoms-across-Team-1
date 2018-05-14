7324613c2493f5e2b20f6d0c9a7f173a92764a6f
DavidValdelvira <37409510+DavidValdelvira@users.noreply.github.com>
2018-05-14 03:01:02 +0200

    Add files via upload

---
+++ Espadachin.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EstadosJuego.CoreGame.drylands;
+
+/**
+ *
+ * @author david
+ */
+public class Espadachin extends Enemigo{
+    
+    public Espadachin(){
+        super();
+    }
+    
+    public void ActualizarEnemigo(){
+        if (this.dist > 0){
+            //actualiza posición del personaje y avanza hacia el
+        }else{
+            //atacar
+        }
+    }
+}
